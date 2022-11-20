16ac2143600f9c8aed1feb5a58736ff5dfcaff31
victorpbp <victorrpeixotoo@gmail.com>
2022-11-20 00:01:31 -0300

    Feat: Adding more methods soon to be implemented and some alrealdy implemented

--- Usuarios.java
+++ Usuarios.java
@@ -5,7 +5,27 @@ public abstract class Usuarios {
     private int tempoDeEmprestimo;
     private State estado;
 
+    private int numReservas;
+
     public void setEstado(State estado) {
         this.estado = estado;
+    }
+
+    public void emprestimoBemSucedido(String tituloLivro, String dataEmprestimo, State status, String dataDevolucao) { //Seção 3.5.b parte 1
+        //Precisa add um objeto no Array de Histórico de Empréstimos que contenha título, data de empréstimo, status(finalizado ou em curso),
+        //data devolucao (realizada ou prevista)
+    }
+
+    public void reservaBemSucedida(String tituloLivro) { //Seção 3.5.b parte 2
+        //Precisa add um objeto no Array de Reservas que contenha tanto o título que tá sendo passado quanto a data atual
+        //a data atual deve vir de um método estático que provavelmente vem da Biblioteca
+    }
+
+
+
+    public String getNome() { return this.nome; }
+
+    public int getNumReservas() {
+        return numReservas;
     }
 }
